3135af8cca028500f7a7a4f3e6d20754ed180910
Vladimir Petrov <55405697+Wamdy18@users.noreply.github.com>
2021-09-25 22:29:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const btnS = document.getElementById('btnS');
 const btnM = document.getElementById('btnM');
 const btnL = document.getElementById('btnL');
 
+const menuBtn = document.getElementById('menuBurger');
+const openMenu = document.getElementById('open-menu');
+
+let isOpenMenu = false;
+
 btnS.addEventListener('click', () => {
 	btnS.classList.add('btn-active');
 	btnM.classList.remove('btn-active');
@@ -18,4 +23,15 @@ btnL.addEventListener('click', () => {
 	btnL.classList.add('btn-active');
 	btnM.classList.remove('btn-active');
 	btnS.classList.remove('btn-active');
+})
+
+menuBtn.addEventListener('click', () => {
+	if (!isOpenMenu) {
+		openMenu.style.display = 'block';
+		isOpenMenu = !isOpenMenu;
+	} else {
+		openMenu.style.display = 'none';
+		isOpenMenu = !isOpenMenu;
+	}
+
 })
